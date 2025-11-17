2a3a694e18bacdf93321da9a8e8360b39b2f99df
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2025-11-17 20:09:05 +0200

    Update DownloadWorker.java

--- app/src/main/java/com/example/secureapp/DownloadWorker.java
+++ app/src/main/java/com/example/secureapp/DownloadWorker.java
@@ -50,8 +50,7 @@ public class DownloadWorker extends Worker {
     // [ ✅✅✅ نهاية: إصلاح خطأ "cannot find symbol" ]
 
     // [ ✅✅ جديد: رابط السيرفر الوسيط الخاص بك ]
-    private static final String API_ENDPOINT = "https://web-production-22bc.up.railway.app/api/get-video-info?youtubeId=";
-
+    private static final String API_ENDPOINT = "https://web-production-3a04a.up.railway.app/api/get-video-info?youtubeId=";
 
     public DownloadWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
