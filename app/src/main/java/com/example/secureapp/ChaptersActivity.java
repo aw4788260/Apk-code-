b712083d4a48a8a95ad0549bd90fb2dfdc849608
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2025-11-29 00:52:23 +0200

    Update ChaptersActivity.java

--- app/src/main/java/com/example/secureapp/ChaptersActivity.java
+++ app/src/main/java/com/example/secureapp/ChaptersActivity.java
@@ -17,6 +17,8 @@ public class ChaptersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // أضف هذا السطر بعد super.onCreate وقبل setContentView
+getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_chapters);
 
         int subjectId = getIntent().getIntExtra("SUBJECT_ID", -1);
