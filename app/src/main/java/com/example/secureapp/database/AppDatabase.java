31c498030dbb5e69b246d94143ca16c04536c4e0
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2025-11-29 19:02:56 +0200

    Update AppDatabase.java

--- app/src/main/java/com/example/secureapp/database/AppDatabase.java
+++ app/src/main/java/com/example/secureapp/database/AppDatabase.java
@@ -3,7 +3,7 @@ package com.example.secureapp.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {SubjectEntity.class, ChapterEntity.class, VideoEntity.class, ExamEntity.class}, version = 4, exportSchema = false)
+@Database(entities = {SubjectEntity.class, ChapterEntity.class, VideoEntity.class, ExamEntity.class}, version = 5, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract SubjectDao subjectDao();
     public abstract ChapterDao chapterDao();
