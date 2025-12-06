019bec46af3d6f939cbb05081ca1bf0638aab3f8
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2025-12-06 14:37:05 +0200

    Create LoginResponse.java

---
+++ app/src/main/java/com/example/secureapp/network/LoginResponse.java
@@ -0,0 +1,8 @@
+package com.example.secureapp.network;
+
+public class LoginResponse {
+    public boolean success;
+    public String userId;
+    public String firstName;
+    public String message; // في حالة الخطأ
+}
